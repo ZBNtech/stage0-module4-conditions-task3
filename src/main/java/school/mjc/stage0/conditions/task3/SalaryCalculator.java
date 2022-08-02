8fd7892a37389f4f061d92f71ae36155b33033fc
schedulerz <thisisabilkair.zhakupov@gmail.com>
2022-08-02 22:50:38 +0600

    Number #1

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,15 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        if (salary < 0){
+            System.out.println("wrong input!");
+        }else if (salary <= 10000){
+            System.out.println(salary - (salary * 0.15));
+        }if (salary > 10000 && salary <= 20000){
+            System.out.println(salary - (salary * 0.18));
+        }else if (salary > 20000){
+            System.out.println(salary - (salary * 0.2));
+        }
     }
 }
+//Basic Salary <= 10000: taxes - 15%. 10000 < Basic Salary <= 20000: taxes - 18% Basic Salary > 20000 : taxes - 20% Basic salary < 0 -> "wrong input!"
